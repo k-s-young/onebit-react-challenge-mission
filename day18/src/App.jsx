45662e6c0a5203e99f50c661235e1c9b330dc519
s.young <gilssong7@gmail.com>
2026-01-13 23:47:32 +0900

    feat: day18 한입 가계부 앱 생성하기, 페이지 라우팅 설정하기

--- day18/src/App.jsx
+++ day18/src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import NewTransaction from "./pages/NewTransaction";
+import EditTransaction from "./pages/EditTransaction";
 
 function App() {
 
   return (
-    <></>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new-transaction" element={<NewTransaction />} />
+      <Route path="/edit-transaction/:id" element={<EditTransaction />} />
+    </Routes>
   )
 }
 
