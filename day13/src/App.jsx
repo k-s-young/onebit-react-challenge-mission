f23131291434207fbe3af0213c4ca50328ad989f
s.young <gilssong7@gmail.com>
2026-01-13 10:08:17 +0900

    feat: day13 원-달러 환율 변환기 만들기

--- day13/src/App.jsx
+++ day13/src/App.jsx
@@ -1,9 +1,45 @@
 import './App.css'
+import { useState } from 'react'
+
+const CurrentInput = ({ label, value, onChange }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input type="number" value={value} onChange={(e) => onChange(e.target.value)} />
+    </div>
+  )
+}
+
+const KRW_TO_USD_RATE = 1300;
 
 function App() {
+  const [amount, setAmount] = useState({
+    krw: 0,
+    usd: 0,
+  });
 
+  const { krw, usd } = amount;
+
+  const onChangeKrw = (value) => {
+    setAmount({
+      krw: value,
+      usd: value / KRW_TO_USD_RATE,
+    });
+  }
+  const onChangeUsd = (value) => {
+    setAmount({
+      krw: value * KRW_TO_USD_RATE,
+      usd: value,
+    });
+  }
   return (
-    <></>
+    <>
+      <h1>환율 변환기 (KRW-USD)</h1>
+      <div>
+        <CurrentInput label="KRW" value={krw} onChange={onChangeKrw} />
+        <CurrentInput label="USD" value={usd} onChange={onChangeUsd} />
+      </div>
+    </>
   )
 }
 
