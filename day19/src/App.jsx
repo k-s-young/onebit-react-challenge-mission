5d01cb585531d900909712f403b8369ca2d198d8
s.young <gilssong7@gmail.com>
2026-01-14 00:10:20 +0900

    feat: day19 한입 가계부 앱 레이아웃 및 상태관리 기능 구현

--- day19/src/App.jsx
+++ day19/src/App.jsx
@@ -1,17 +1,103 @@
 import './App.css'
+import { useReducer, createContext, useRef, useCallback, useMemo } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import NewTransaction from "./pages/NewTransaction";
 import EditTransaction from "./pages/EditTransaction";
 
+
+const mockData = [
+  {
+    id: 0,
+    name: "마라탕 & 꿔바로우",
+    amount: 59000,
+    type: "expense",
+    category: "🍚 식비",
+    date: new Date().getTime() + 1,
+  },
+  {
+    id: 1,
+    name: "월세",
+    amount: 500000,
+    type: "expense",
+    category: "🏠 생활",
+    date: new Date().getTime() + 2,
+  },
+  {
+    id: 2,
+    name: "월급",
+    amount: 3500000,
+    type: "income",
+    category: "🏢 급여",
+    date: new Date().getTime() + 3,
+  },
+];
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'INIT':
+      return action.data;
+    case 'CREATE':
+      return [...state, action.data];
+    case 'UPDATE':
+      return state.map((transaction) => 
+        String(transaction.id) === String(action.data.id) ? action.data : transaction
+      );
+    case 'DELETE':
+      return state.filter((transaction) => String(transaction.id) !== String(action.data.id));
+    default:
+      return state;
+  }
+}
+
+export const TransactionStateContext = createContext();
+export const TransactionDispatchContext = createContext();
+
 function App() {
+  const [transactions, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(mockData.length);
+
+  const onCreateTransaction = useCallback((name, amount, type, category, date) => {
+    dispatch({ 
+      type: 'CREATE', 
+      data: {
+        id: idRef.current++,
+        name, amount, type, category, date,
+      }
+    });
+  }, []);
+
+  const onUpdateTransaction = useCallback((id, name, amount, type, category, date) => {
+    dispatch({ 
+      type: 'UPDATE', 
+      data: { 
+        id, name, amount, type, category, date,
+      }
+    });
+  }, []);
+
+  const onDeleteTransaction = useCallback((id) => {
+    dispatch({ type: 'DELETE', id });
+  }, []); 
+
+  const memoizedDispatch = useMemo(() => {
+    return {
+      onCreateTransaction,
+      onUpdateTransaction,
+      onDeleteTransaction,
+    }
+  }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new-transaction" element={<NewTransaction />} />
-      <Route path="/edit-transaction/:id" element={<EditTransaction />} />
-    </Routes>
+    <TransactionStateContext.Provider value={transactions}>
+      <TransactionDispatchContext.Provider value={memoizedDispatch}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new-transaction" element={<NewTransaction />} />
+          <Route path="/edit-transaction/:id" element={<EditTransaction />} />
+        </Routes>
+      </TransactionDispatchContext.Provider>
+    </TransactionStateContext.Provider>
   )
 }
 
